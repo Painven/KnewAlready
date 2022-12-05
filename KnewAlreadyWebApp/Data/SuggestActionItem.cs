62a7ef292da9aee68cdc4e281be1a11f49a958b6
Дмитрий Смирнов <painven@gmail.com>
2022-12-06 02:34:40 +0300

    UI главной доработка

---
+++ KnewAlreadyWebApp/Data/SuggestActionItem.cs
@@ -0,0 +1,10 @@
+﻿namespace KnewAlreadyWebApp.Data;
+
+public class SuggestActionItem
+{
+    public string InitiatorUsername { get; init; }
+    public string AcceptorUsername { get; init; }
+    public string CategoryName { get; init; }
+    public DateTime Created { get; init; }
+    public bool IsCompleted { get; init; }
+}
